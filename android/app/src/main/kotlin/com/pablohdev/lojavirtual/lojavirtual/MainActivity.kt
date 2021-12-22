46df5defade3cf5c0353b4971813b5feced93fde
Pablo Henrique <pablo.fracasso@sthima.com.br>
2021-12-21 23:49:35 -0300

    configure firebase

---
+++ android/app/src/main/kotlin/com/pablohdev/lojavirtual/lojavirtual/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pablohdev.lojavirtual.lojavirtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Loja Virtual',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+          backgroundColor: Colors.white,
+          body: const Center(
+            child: Text('Teste'),
+          )),
+    );
+  }
+}
